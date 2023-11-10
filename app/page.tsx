640989cce7f4af01bc3a2f075f11d76b0378ec04
AlexT <alex.thummas66@gmail.com>
2023-11-10 15:35:13 -0500

    Updated directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import 'styles/index.css';
+import { Button } from '@mantine/core';
+import "@fontsource/open-sans";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return <div>
+    Home Page
+    <Button variant="filled">Button</Button>
+  </div>;
 }
