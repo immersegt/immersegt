7a34e8ccf29be46744c6db96b69e4ecd55aa939b
AlexT <alex.thummas66@gmail.com>
2023-11-27 21:01:17 -0500

    Added features section on landing page using Mantine UI, added animate on scroll

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ import '../styles/index.css'
 import Header from '../components/Header';
 import Sponsors from '../components/Sponsors';
 import Tracks from '../components/Tracks';
-import Conclusion from '../components/Conclusion';
+import Sell from '../components/Sell';
 import Judges from '../components/Judges';
 import FAQ from '../components/FAQ';
+import Conclusion from '../components/Conclusion';
 
 const HomePage = () => {
   return (
@@ -16,6 +17,7 @@ const HomePage = () => {
         <Sponsors/>
         <a id="tracks"/>
         <Tracks/>
+        <Sell/>
         <Judges/>
         <FAQ/>
         <Conclusion/>
---
+++ components/Sell.tsx
@@ -0,0 +1,82 @@
+import '../styles/index.css';
+import '../styles/sell.css';
+
+import {
+    Badge,
+    Group,
+    Title,
+    Text,
+    Card,
+    SimpleGrid,
+    Container,
+    rem,
+    useMantineTheme,
+    Divider
+} from '@mantine/core';
+import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
+
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const mockdata = [
+    {
+        title: 'Networking',
+        description:
+            "At ImmerseGT, you will be interacting with other college students like yourself — innovators, builders, engineers, coders, entrepreneurs, and more.",
+        icon: IconGauge,
+        delay: 0
+    },
+    {
+        title: 'Workshops',
+        description:
+            "No experience required — even as a beginner, you can get started in the world of XR by attending our many training workshops throughout the weekend.",
+        icon: IconUser,
+        delay: 100
+    },
+    {
+        title: 'Resources',
+        description:
+            'All you need is a laptop and an idea. Solo entrants can find teammates at ImmerseGT and we will provide the headsets, tools, and space you need to hack effectively.',
+        icon: IconCookie,
+        delay: 200
+    },
+];
+
+const Sell = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    const theme = useMantineTheme();
+    const features = mockdata.map((feature) => (
+        <Card key={feature.title} shadow="md" radius="md" className="card" padding="xl" data-aos="fade-up" data-aos-easing="ease-sine" data-aos-anchor-placement="center-bottom" data-aos-once="true" data-aos-delay={feature.delay}>
+            <feature.icon
+                style={{ width: rem(50), height: rem(50) }}
+                stroke={2}
+                color={theme.colors.grape[5]}
+            />
+            <Text fz="lg" fw={500} className="cardTitle" mt="md">
+                {feature.title}
+            </Text>
+            <Text fz="sm" c="dimmed" mt="sm">
+                {feature.description}
+            </Text>
+        </Card>
+    ));
+
+    return (
+        <section className="sellContainer">
+            <h2>
+                ImmerseGT is More Than Just Coding
+            </h2>
+
+            <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl">
+                {features}
+            </SimpleGrid>
+        </section>
+    );
+}
+
+export default Sell;
