9d24506de04d65b7d618d3ba280abb7b291cf847
AlexT <alex.thummas66@gmail.com>
2023-11-30 12:35:59 -0500

    Rearranged schedule page

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -12,15 +12,45 @@ const endDate = "2024-04-21T08:00:00";
 
 const Schedule = () => {
     return (
-        <section>
-            Schedule Page
-            <Countdown target1={startDate} target2={endDate}/>
-            <div className="scheduleContainer">
-                <ScheduleItem title="Opening Ceremony" time="5:00PM" description="Join us for an interactive and engaging
+        <div className="schedule">
+            <aside className="scheduleAside">
+                <h3>Countdown</h3>
+                <Countdown target1={startDate} target2={endDate} />
+                <br />
+                <h3>Hackathon Schedule</h3>
+                <div className="scheduleContainer2">
+                    <p>November 10th</p>
+                    <ScheduleItem title="Opening Ceremony" time="5:00PM" description="Join us for an interactive and engaging
                 workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
-            </div>
-            <EventCard title="UX Workshop" time="5:00PM" location="KLAUS 1234" description="Join us for an interactive and engaging workshop with industry professionals as we learn to design UI. This workshop will be hosted by GTXR and is targeted to beginners and experts alike." soon={true}/>
-        </section>
+                    <ScheduleItem title="Hacking Starts" time="7:00PM" description="Join us for an interactive and engaging
+                workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
+                    <br />
+                    <p>November 12th</p>
+                    <ScheduleItem title="Projects Due" time="9:00AM" description="Join us for an interactive and engaging
+                workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
+                    <ScheduleItem title="Judging" time="10:00AM" description="Join us for an interactive and engaging
+                workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
+                    <ScheduleItem title="Closing Ceremony" time="12:00PM" description="Join us for an interactive and engaging
+                workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
+                </div>
+            </aside>
+            <div className="verticalRule5" />
+            <main className="scheduleMain">
+                <h3>Upcoming Events</h3>
+                <div className="eventContainer">
+                    <EventCard title="UX Workshop" time="5:00PM" location="KLAUS 1234" description="Join us for an interactive and engaging workshop with industry professionals as we learn to design UI. This workshop will be hosted by GTXR and is targeted to beginners and experts alike." soon={true} />
+                    <EventCard title="UX Workshop" time="5:00PM" location="KLAUS 1234" description="Join us for an interactive and engaging workshop with industry professionals as we learn to design UI. This workshop will be hosted by GTXR and is targeted to beginners and experts alike." soon={true} />
+                </div>
+                <br />
+                <h3>Event Schedule</h3>
+                <div className="scheduleContainer">
+                    <p>November 10th</p>
+                    <ScheduleItem title="UX Workshop" time="5:00PM" description="Join us for an interactive and engaging
+                workshop with industry professionals where we will" save={true} location="KLAUS 1234" />
+                </div>
+
+            </main>
+        </div>
     )
 }
 
