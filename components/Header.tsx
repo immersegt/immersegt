0c0c26a9c010327203cb7c2f771a55eca6492dbd
AlexT <alex.thummas66@gmail.com>
2023-12-14 13:08:26 -0500

    Refreshed header on landing page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 import '../styles/index.css';
 import '../styles/header.css';
 
-import GogglesImg from '../public/goggles.png';
-import {Button} from '@mantine/core';
+import GogglesImg from '../public/Dalle.png';
+import { UnstyledButton } from '@mantine/core';
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Header = () => {
@@ -10,14 +10,14 @@ const Header = () => {
         <section className="homeHeader">
             <div className="homeInfo">
                 <span className="homeSubtitle">World's Largest XR Hackathon</span>
-                <h1 className="homeTitle">Hack with us at <br/><span className="homeTitleEmphasis">ImmerseGT</span></h1>
+                <h1 className="homeTitle">Hack with us at <br /><span className="homeTitleEmphasis">ImmerseGT</span></h1>
                 <p className="homeDescription">Participate in ImmerseGT, an innovative XR hackathon hosted by the Startup Exchange and GTXR, on Georgia Tech's campus. Compete for your share of over $20,000 in prizes as you build XR applications using state-of-the-art headsets.</p>
                 <div className="buttonGroup">
-                <Button className="homeButton" variant="gradient" gradient={{ from: 'violet', to: 'grape', deg: 95 }} size="xl" component="a" href="/account">Get Started&nbsp;<FaLongArrowAltRight /></Button>
-                <Button className="homeButton" variant="outline" color="grape.5" size="xl" component="a" href="#tracks">Learn More</Button>
+                    <UnstyledButton className="redirectButton main" component="a" href="/account">Get Started&nbsp;<FaLongArrowAltRight /></UnstyledButton>
+                    <UnstyledButton className="redirectButton" component="a" href="#tracks">Learn More</UnstyledButton>
                 </div>
             </div>
-            <img src={GogglesImg.src} width="600px" className="homeImage" />
+            <img src={GogglesImg.src} width="640px" className="homeImage" />
         </section>
     )
 }
