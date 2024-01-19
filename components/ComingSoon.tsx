c9bd0c8d0f7a6eb9be0f04ed181353b95358d52c
AlexT <alex.thummas66@gmail.com>
2024-01-19 13:26:49 -0500

    Removed team features for initial website deployment

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,10 +1,12 @@
 import '../styles/registered.css';
+import '../styles/comingsoon.css';
 import '../styles/index.css';
 
 import Link from "next/link";
 
 const ComingSoon = () => {
     return (
+        <div className="container">
         <div className="registeredBox">
             <div className="registeredTitle">
                 Coming Soon.
@@ -12,6 +14,7 @@ const ComingSoon = () => {
             <p className="registeredLargeText">Thank you for your interest in ImmerseGT! We are currently accepting applications for our 2024 hackathon. You will be able to access this page
             if accepted. If you haven't applied yet, make sure to <Link href="/apply">apply here.</Link></p>
         </div>
+         </div>
     )
 }
 
