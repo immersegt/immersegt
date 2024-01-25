df3be9d3348a0b09a48d1a85d3646656e1ff134f
Barrarroso <correodelnino@gmail.com>
2024-01-24 22:10:29 -0500

    Updated all packages and imports

--- components/DiscordButton.tsx
+++ components/DiscordButton.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonProps } from '@mantine/core';
-import { DiscordIcon } from '@mantine/ds';
+import { DiscordIcon } from '@mantinex/dev-icons';
 
 export function DiscordButton(props: ButtonProps & React.ComponentPropsWithoutRef<'button'>) {
   return <Button leftSection={<DiscordIcon width='1rem' height='1rem' />} variant="default" {...props} />;
