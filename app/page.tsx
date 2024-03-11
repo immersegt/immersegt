eed776983942ba4acd29d902293fe86d38ab689f
AlexT <alex.thummas66@gmail.com>
2024-03-11 18:23:42 -0400

    remove mlh banner

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,6 @@ import Conclusion from '../components/Conclusion';
 const HomePage = () => {
   return (
     <main>
-      <a id="mlh-trust-badge" className="mlhTrustBadge" href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=black" target="_blank">
-        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg" alt="Major League Hacking 2024 Hackathon Season" />
-      </a>
       <Header />
       <a id="sponsors" />
       <Sponsors />
